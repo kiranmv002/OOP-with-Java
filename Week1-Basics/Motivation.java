b39dd03a825ebf471fd0689730bfd10265d98b83
M V Kiran <mada.kiran@s.amity.edu>
2026-03-05 17:55:09 +0530

    Update and rename HelloWorld.java to Motivation.java

---
+++ Week1-Basics/Motivation.java
@@ -0,0 +1,15 @@
+// Day 1 - Simple Java Program
+// This program prints a motivational message
+// System.out.println prints the output on screen
+
+public class Motivation {
+    public static void main(String[] args) {
+
+        System.out.println("Good Morning!");
+        System.out.println("Believe in yourself.");
+        System.out.println("Practice daily.");
+        System.out.println("Stay consistent.");
+        System.out.println("Success will come!");
+
+    }
+}
