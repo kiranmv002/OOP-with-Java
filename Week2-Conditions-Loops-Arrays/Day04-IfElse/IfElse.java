aed65fb5e9ce78108a787f267c8a2ae93e1ac2a8
M V Kiran <mada.kiran@s.amity.edu>
2026-03-09 17:34:14 +0530

    Update IfElse.java

--- Week2-Conditions-Loops-Arrays/Day04-IfElse/IfElse.java
+++ Week2-Conditions-Loops-Arrays/Day04-IfElse/IfElse.java
@@ -1,1 +1,22 @@
+// day 4 - if else
+// basic condition checking
+// if condition is true it runs that block, else other block
 
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("positive number");
+        } else if (num < 0) {
+            System.out.println("negative number");
+        } else {
+            System.out.println("its zero");
+        }
+    }
+}
