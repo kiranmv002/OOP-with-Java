de876021a3cf178bf4a3482bbe7907ac4f6a3f15
M V Kiran <mada.kiran@s.amity.edu>
2026-03-13 22:46:45 +0530

    Update SimpleCalculator.java

--- Week2-Conditions-Loops-Arrays/Day05-Switch/SimpleCalculator.java
+++ Week2-Conditions-Loops-Arrays/Day05-Switch/SimpleCalculator.java
@@ -1,3 +1,17 @@
 // day 5 - simple calculator using switch
 // user enters two numbers and an operator
 // switch checks which operator and does the calculation
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0); // reading single character
+
+        System.out.print("enter second number: ");
+        double b = sc.nextDouble();
