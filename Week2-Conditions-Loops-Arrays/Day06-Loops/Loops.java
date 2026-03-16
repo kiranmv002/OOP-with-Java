ee53f7f929e007f0485744977f45fdd4e882bfc4
M V Kiran <mada.kiran@s.amity.edu>
2026-03-16 21:07:15 +0530

    Update Loops.java

--- Week2-Conditions-Loops-Arrays/Day06-Loops/Loops.java
+++ Week2-Conditions-Loops-Arrays/Day06-Loops/Loops.java
@@ -1,1 +1,5 @@
-
+// day 6 - loops
+// three types of loops in java
+// for loop - when we know how many times
+// while loop - when we dont know how many times
+// do while - runs at least once even if condition is false
