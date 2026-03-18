391192d3b437d9a5a2178ae3a29d6c6a389b648c
M V Kiran <mada.kiran@s.amity.edu>
2026-03-18 21:03:55 +0530

    Update PrimeNumber.java

--- Week2-Conditions-Loops-Arrays/Day06-Loops/PrimeNumber.java
+++ Week2-Conditions-Loops-Arrays/Day06-Loops/PrimeNumber.java
@@ -2,3 +2,32 @@
 // prime = number divisible only by 1 and itself
 // checking from 2 to n/2 is enough, no need to go till n
 
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) { // if divisible by anything its not prime
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is prime");
+        } else {
+            System.out.println(n + " is not prime");
+        }
+    }
+}
