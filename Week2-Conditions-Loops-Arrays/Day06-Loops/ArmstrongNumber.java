a1ebf88f09cb17177c37db4de1bc3e77817b7b0a
M V Kiran <mada.kiran@s.amity.edu>
2026-03-21 22:37:14 +0530

    Update ArmstrongNumber.java

--- Week2-Conditions-Loops-Arrays/Day06-Loops/ArmstrongNumber.java
+++ Week2-Conditions-Loops-Arrays/Day06-Loops/ArmstrongNumber.java
@@ -2,3 +2,29 @@
 // armstrong = sum of cubes of digits equals the number
 // example: 153 = 1^3 + 5^3 + 3^3 = 1+125+27 = 153
 // this one was fun to figure out
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10; // get last digit
+            sum = sum + (digit * digit * digit); // cube it and add
+            n = n / 10; // remove last digit
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an armstrong number");
+        } else {
+            System.out.println(original + " is not an armstrong number");
+        }
+    }
+}
