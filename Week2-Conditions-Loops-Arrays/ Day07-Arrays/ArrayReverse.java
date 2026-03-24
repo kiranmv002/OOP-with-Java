ee1b027aac8350a32f7f3777e3ade96462b0cb93
M V Kiran <mada.kiran@s.amity.edu>
2026-03-24 22:19:55 +0530

    Update ArrayReverse.java

--- Week2-Conditions-Loops-Arrays/ Day07-Arrays/ArrayReverse.java
+++ Week2-Conditions-Loops-Arrays/ Day07-Arrays/ArrayReverse.java
@@ -1,2 +1,25 @@
 // day 7 - reverse an array
 // printing array from last index to first index
+
+import java.util.Scanner;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("how many elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("enter element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("reversed: ");
+        for (int i = n - 1; i >= 0; i--) { // start from last index
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
