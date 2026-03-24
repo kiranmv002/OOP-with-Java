d373eed927c10c44e4a4e30e3d3c50137fb65df9
M V Kiran <mada.kiran@s.amity.edu>
2026-03-24 22:16:51 +0530

    Update LargestInArray.java

--- Week2-Conditions-Loops-Arrays/ Day07-Arrays/LargestInArray.java
+++ Week2-Conditions-Loops-Arrays/ Day07-Arrays/LargestInArray.java
@@ -1,3 +1,29 @@
 // day 7 - largest element in array
 // assuming first element is largest
 // then comparing with each element
+
+import java.util.Scanner;
+
+public class LargestInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("how many elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("enter element: ");
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0]; // assume first is largest
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i]; // update if bigger found
+            }
+        }
+
+        System.out.println("largest = " + largest);
+    }
+}
