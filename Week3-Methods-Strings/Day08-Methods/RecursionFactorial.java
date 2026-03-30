308d157b2757c8314ce3f2490f6d9f445a836359
M V Kiran <mada.kiran@s.amity.edu>
2026-03-30 17:53:29 +0530

    Create RecursionFactorial.java

---
+++ Week3-Methods-Strings/Day08-Methods/RecursionFactorial.java
@@ -0,0 +1,6 @@
+// day 8 - recursion
+// recursion = method calling itself
+// factorial using recursion
+// factorial(5) = 5 * factorial(4)
+//              = 5 * 4 * factorial(3) and so on
+// base case is important, without it it runs forever
