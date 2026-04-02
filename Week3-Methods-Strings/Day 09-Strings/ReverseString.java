d49934479e9091cc74e934e5bc5199a506f38e74
M V Kiran <mada.kiran@s.amity.edu>
2026-04-02 17:54:34 +0530

    Update ReverseString.java

--- Week3-Methods-Strings/Day 09-Strings/ReverseString.java
+++ Week3-Methods-Strings/Day 09-Strings/ReverseString.java
@@ -1,3 +1,21 @@
 // day 9 - reverse a string
 // looping from last character to first
 // building a new reversed string
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a string: ");
+        String str = sc.next();
+
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        System.out.println("reversed: " + reversed);
+    }
+}
