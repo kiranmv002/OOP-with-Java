333ec7b36fb9dd4f6a3b01c23e0df8579e33e543
M V Kiran <mada.kiran@s.amity.edu>
2026-04-05 21:15:46 +0530

    Create AnagramCheck.java

---
+++ Week3-Methods-Strings/Day 09-Strings/AnagramCheck.java
@@ -0,0 +1,5 @@
+// day 9 - anagram check
+// anagram = two words with same characters in different order
+// example: listen and silent are anagrams
+// trick: sort both strings and compare
+// if sorted versions are equal then anagram
