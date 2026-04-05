3fd710990e1d2e11c5ce370dbe71b8fa2c84a784
M V Kiran <mada.kiran@s.amity.edu>
2026-04-05 21:13:04 +0530

    Update CountVowels.java

--- Week3-Methods-Strings/Day 09-Strings/CountVowels.java
+++ Week3-Methods-Strings/Day 09-Strings/CountVowels.java
@@ -1,3 +1,25 @@
 // day 9 - count vowels in a string
 // vowels are a, e, i, o, u
 // looping through each character and checking if its a vowel
+
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a string: ");
+        String str = sc.next().toLowerCase();
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // checking if character is a vowel
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("number of vowels = " + count);
+    }
+}
