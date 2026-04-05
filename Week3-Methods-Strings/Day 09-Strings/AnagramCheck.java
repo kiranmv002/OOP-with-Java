605c49a39f5944e91cb15b75509aaa2ecf618519
M V Kiran <mada.kiran@s.amity.edu>
2026-04-05 21:18:22 +0530

    Update AnagramCheck.java

--- Week3-Methods-Strings/Day 09-Strings/AnagramCheck.java
+++ Week3-Methods-Strings/Day 09-Strings/AnagramCheck.java
@@ -3,3 +3,31 @@
 // example: listen and silent are anagrams
 // trick: sort both strings and compare
 // if sorted versions are equal then anagram
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter first word: ");
+        String word1 = sc.next().toLowerCase();
+
+        System.out.print("enter second word: ");
+        String word2 = sc.next().toLowerCase();
+
+        // convert to char array, sort, then compare
+        char[] arr1 = word1.toCharArray();
+        char[] arr2 = word2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("yes they are anagrams");
+        } else {
+            System.out.println("no they are not anagrams");
+        }
+    }
+}
