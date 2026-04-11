e95de731beb197310729c7b4f0eefdc534d10027
M V Kiran <mada.kiran@s.amity.edu>
2026-04-11 23:31:17 +0530

    Update Student.java

--- Week4-OOP-Basics/Student.java
+++ Week4-OOP-Basics/Student.java
@@ -1,2 +1,5 @@
 // day 10 - classes and objects
 // class is a blueprint, object is the actual thing
+// like class is the design of a house, object is the actual house
+// variables inside class are called fields or attributes
+// functions inside class are called methods
