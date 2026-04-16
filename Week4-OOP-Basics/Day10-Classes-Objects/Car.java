2fef417d50f5bb4d5f4852c831a99988d0db4d34
M V Kiran <mada.kiran@s.amity.edu>
2026-04-16 18:20:22 +0530

    Update Car.java

--- Week4-OOP-Basics/Day10-Classes-Objects/Car.java
+++ Week4-OOP-Basics/Day10-Classes-Objects/Car.java
@@ -2,3 +2,43 @@
 // another real world example
 // car has attributes like brand, speed
 // and methods like accelerate and brake
+
+public class Car {
+
+    String brand;
+    String color;
+    int speed;
+
+    void accelerate(int amount) {
+        speed = speed + amount;
+        System.out.println(brand + " speed increased to " + speed);
+    }
+
+    void brake(int amount) {
+        if (speed - amount < 0) {
+            speed = 0;
+        } else {
+            speed = speed - amount;
+        }
+        System.out.println(brand + " speed reduced to " + speed);
+    }
+
+    void display() {
+        System.out.println("brand: " + brand);
+        System.out.println("color: " + color);
+        System.out.println("speed: " + speed);
+    }
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.brand = "toyota";
+        c1.color = "red";
+        c1.speed = 0;
+
+        c1.display();
+        System.out.println();
+        c1.accelerate(50);
+        c1.accelerate(30);
+        c1.brake(20);
+    }
+}
