b26ce33ca4fdec1f1287b298afc375f36916cc6b
M V Kiran <mada.kiran@s.amity.edu>
2026-04-16 18:12:02 +0530

    Update BankAccount.java

--- Week4-OOP-Basics/Day10-Classes-Objects/BankAccount.java
+++ Week4-OOP-Basics/Day10-Classes-Objects/BankAccount.java
@@ -2,3 +2,29 @@
 // real world example of class and object
 // deposit and withdraw are methods that change balance
 // balance should not go negative
+
+public class BankAccount {
+
+    String owner;
+    double balance;
+
+    void deposit(double amount) {
+        balance = balance + amount;
+        System.out.println("deposited: " + amount);
+        System.out.println("balance: " + balance);
+    }
+
+    void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("not enough balance!");
+        } else {
+            balance = balance - amount;
+            System.out.println("withdrawn: " + amount);
+            System.out.println("balance: " + balance);
+        }
+    }
+
+    void display() {
+        System.out.println("account owner: " + owner);
+        System.out.println("current balance: " + balance);
+    }
