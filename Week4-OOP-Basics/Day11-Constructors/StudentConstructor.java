d2cfa8c80c4a748f29bdb69f672ac1d2ca13f093
M V Kiran <mada.kiran@s.amity.edu>
2026-04-16 18:21:26 +0530

    Create StudentConstructor.java

---
+++ Week4-OOP-Basics/Day11-Constructors/StudentConstructor.java
@@ -0,0 +1,5 @@
+// day 11 - constructors
+// constructor is a special method that runs when object is created
+// same name as class, no return type
+// used to set initial values when creating object
+// default constructor has no parameters
