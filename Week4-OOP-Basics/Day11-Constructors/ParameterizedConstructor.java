c334ad9a7667fc9ce7fc83646021ba26f9d9c4c7
M V Kiran <mada.kiran@s.amity.edu>
2026-04-19 20:50:07 +0530

    Update ParameterizedConstructor.java

--- Week4-OOP-Basics/Day11-Constructors/ParameterizedConstructor.java
+++ Week4-OOP-Basics/Day11-Constructors/ParameterizedConstructor.java
@@ -3,3 +3,23 @@
 // we can pass values when creating object
 // cleaner than setting values one by one
 // this keyword refers to current object
+
+public class ParameterizedConstructor {
+
+    String name;
+    int age;
+    float marks;
+
+    // parameterized constructor
+    ParameterizedConstructor(String name, int age, float marks) {
+        this.name = name;     // this.name = field, name = parameter
+        this.age = age;
+        this.marks = marks;
+    }
+
+    void display() {
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("marks: " + marks);
+    }
+
