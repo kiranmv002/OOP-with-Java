4c419edefeaa959648c0a55d866e28fbe15f62f4
M V Kiran <mada.kiran@s.amity.edu>
2026-04-19 20:47:59 +0530

    Update StudentConstructor.java

--- Week4-OOP-Basics/Day11-Constructors/StudentConstructor.java
+++ Week4-OOP-Basics/Day11-Constructors/StudentConstructor.java
@@ -3,3 +3,28 @@
 // same name as class, no return type
 // used to set initial values when creating object
 // default constructor has no parameters
+
+public class StudentConstructor {
+
+    String name;
+    int age;
+    float marks;
+
+    // default constructor - no parameters
+    StudentConstructor() {
+        name = "unknown";
+        age = 0;
+        marks = 0;
+        System.out.println("default constructor called");
+    }
+
+    void display() {
+        System.out.println("name: " + name + " age: " + age + " marks: " + marks);
+    }
+
+    public static void main(String[] args) {
+        // default constructor is called automatically
+        StudentConstructor s1 = new StudentConstructor();
+        s1.display();
+    }
+}
