e49131e58f55931d9eddb4823dc783ae21f6bb51
M V Kiran <mada.kiran@s.amity.edu>
2026-04-19 20:48:22 +0530

    Create ParameterizedConstructor.java

---
+++ Week4-OOP-Basics/Day11-Constructors/ParameterizedConstructor.java
@@ -0,0 +1,5 @@
+// day 11 - parameterized constructor
+// constructor with parameters
+// we can pass values when creating object
+// cleaner than setting values one by one
+// this keyword refers to current object
