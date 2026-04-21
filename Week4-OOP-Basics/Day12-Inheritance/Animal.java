4305bb5b7b7aa983c58199b97de581c85055f9f3
M V Kiran <mada.kiran@s.amity.edu>
2026-04-21 22:04:54 +0530

    Update Animal.java

--- Week4-OOP-Basics/Day12-Inheritance/Animal.java
+++ Week4-OOP-Basics/Day12-Inheritance/Animal.java
@@ -3,3 +3,19 @@
 // avoids writing same code again
 // extends keyword is used for inheritance
 // parent class = Animal, child classes = Dog, Cat
+
+public class Animal {
+
+    String name;
+    String sound;
+
+    // parent class method
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    void makeSound() {
+        System.out.println(name + " says " + sound);
+    }
+}
+
