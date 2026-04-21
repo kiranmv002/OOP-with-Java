19d64e3dd6e07fc0f2a42dfa603cb069c6c45345
M V Kiran <mada.kiran@s.amity.edu>
2026-04-21 22:03:18 +0530

    Create Animal.java

---
+++ Week4-OOP-Basics/Day12-Inheritance/Animal.java
@@ -0,0 +1,5 @@
+// day 12 - inheritance
+// inheritance = child class gets all properties of parent class
+// avoids writing same code again
+// extends keyword is used for inheritance
+// parent class = Animal, child classes = Dog, Cat
