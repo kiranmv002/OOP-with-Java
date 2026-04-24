cd8d2f032a7d5c056aa022b490ab28e4043dfc61
M V Kiran <mada.kiran@s.amity.edu>
2026-04-24 22:07:35 +0530

    Update Employee.java

--- Week4-OOP-Basics/Day12-Inheritance/Employee.java
+++ Week4-OOP-Basics/Day12-Inheritance/Employee.java
@@ -3,3 +3,38 @@
 // manager has extra teamSize field
 // developer has extra language field
 // both inherit salary and name from Employee
+
+public class Employee {
+
+    String name;
+    double salary;
+    String department;
+
+    void displayInfo() {
+        System.out.println("name: " + name);
+        System.out.println("department: " + department);
+        System.out.println("salary: " + salary);
+    }
+
+    void work() {
+        System.out.println(name + " is working");
+    }
+}
+
+class Manager extends Employee {
+
+    int teamSize;
+
+    void manage() {
+        System.out.println(name + " is managing a team of " + teamSize);
+    }
+}
+
+class Developer extends Employee {
+
+    String language;
+
+    void code() {
+        System.out.println(name + " is coding in " + language);
+    }
+}
