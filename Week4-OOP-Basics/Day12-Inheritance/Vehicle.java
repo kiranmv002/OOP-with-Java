c5d8efea9f873d2d41104605f0e4745600eb8845
M V Kiran <mada.kiran@s.amity.edu>
2026-04-24 21:43:08 +0530

    Create Vehicle.java

---
+++ Week4-OOP-Basics/Day12-Inheritance/Vehicle.java
@@ -0,0 +1,5 @@
+// day 12 - vehicle inheritance
+// Vehicle is parent class
+// Car and Bike are child classes
+// child classes inherit start() and stop() from Vehicle
+// and have their own methods too
