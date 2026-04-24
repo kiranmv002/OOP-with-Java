edc91fbbf73376e80d6e6ff471f2a76b0f8bdfbc
M V Kiran <mada.kiran@s.amity.edu>
2026-04-24 21:53:46 +0530

    Update Vehicle.java

--- Week4-OOP-Basics/Day12-Inheritance/Vehicle.java
+++ Week4-OOP-Basics/Day12-Inheritance/Vehicle.java
@@ -3,3 +3,27 @@
 // Car and Bike are child classes
 // child classes inherit start() and stop() from Vehicle
 // and have their own methods too
+
+public class Vehicle {
+
+    String brand;
+    int speed;
+
+    void start() {
+        System.out.println(brand + " started");
+    }
+
+    void stop() {
+        System.out.println(brand + " stopped");
+    }
+}
+
+class Car2 extends Vehicle {
+
+    int doors;
+
+    void honk() {
+        System.out.println(brand + " is honking");
+    }
+}
+
