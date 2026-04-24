c39746915c7c92459e929bd7c3de59b0414a223e
M V Kiran <mada.kiran@s.amity.edu>
2026-04-24 22:03:33 +0530

    Document inheritance structure in Employee.java
    Added comments to explain inheritance structure.

---
+++ Week4-OOP-Basics/Day12-Inheritance/Employee.java
@@ -0,0 +1,5 @@
+// day 12 - employee inheritance
+// Employee is parent, Manager and Developer are children
+// manager has extra teamSize field
+// developer has extra language field
+// both inherit salary and name from Employee
