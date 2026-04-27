25f06b5ed2d0379b8cff1ff9ec2c55d8f2298257
M V Kiran <mada.kiran@s.amity.edu>
2026-04-27 21:23:27 +0530

    Create RuntimePolymorphism.java

---
+++ Week4-OOP-Basics/Day13-Polymorphism/RuntimePolymorphism.java
@@ -0,0 +1,4 @@
+// day 13 - runtime polymorphism
+// parent reference pointing to child object
+// java decides at runtime which method to call
+// this was a bit confusing at first but makes sense now
