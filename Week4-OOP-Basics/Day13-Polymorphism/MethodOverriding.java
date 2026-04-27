dbde66ba34a96a7e6f850dc1f3cd02ab508facdb
M V Kiran <mada.kiran@s.amity.edu>
2026-04-27 21:22:55 +0530

    Update MethodOverriding.java

--- Week4-OOP-Basics/Day13-Polymorphism/MethodOverriding.java
+++ Week4-OOP-Basics/Day13-Polymorphism/MethodOverriding.java
@@ -2,3 +2,40 @@
 // child class redefines a method from parent class
 // same method name and parameters
 // this is runtime polymorphism
+
+class Shape {
+
+    void draw() {
+        System.out.println("drawing a shape");
+    }
+}
+
+class Circle extends Shape {
+
+    // overriding parent method
+    @Override
+    void draw() {
+        System.out.println("drawing a circle");
+    }
+}
+
+class Rectangle extends Shape {
+
+    @Override
+    void draw() {
+        System.out.println("drawing a rectangle");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Shape s = new Shape();
+        s.draw(); // calls Shape's draw
+
+        Circle c = new Circle();
+        c.draw(); // calls Circle's draw
+
+        Rectangle r = new Rectangle();
+        r.draw(); // calls Rectangle's draw
+    }
+}
