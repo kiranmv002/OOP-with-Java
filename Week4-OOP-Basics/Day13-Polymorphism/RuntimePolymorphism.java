a22cb2009dae5a0baa62a77e03fd96d91cfe5d6d
M V Kiran <mada.kiran@s.amity.edu>
2026-04-27 21:27:52 +0530

    Update RuntimePolymorphism.java

--- Week4-OOP-Basics/Day13-Polymorphism/RuntimePolymorphism.java
+++ Week4-OOP-Basics/Day13-Polymorphism/RuntimePolymorphism.java
@@ -2,3 +2,23 @@
 // parent reference pointing to child object
 // java decides at runtime which method to call
 // this was a bit confusing at first but makes sense now
+
+class Animal2 {
+    void sound() {
+        System.out.println("animal makes a sound");
+    }
+}
+
+class Dog2 extends Animal2 {
+    @Override
+    void sound() {
+        System.out.println("dog says woof");
+    }
+}
+
+class Cat2 extends Animal2 {
+    @Override
+    void sound() {
+        System.out.println("cat says meow");
+    }
+}
