222b5b99977aacfa7a01f4a76c3ade30a23e8c5f
M V Kiran <mada.kiran@s.amity.edu>
2026-04-27 21:17:33 +0530

    Update MethodOverloading.java

--- Week4-OOP-Basics/Day13-Polymorphism/MethodOverloading.java
+++ Week4-OOP-Basics/Day13-Polymorphism/MethodOverloading.java
@@ -2,3 +2,25 @@
 // same method name but different parameters
 // java decides which one to call based on what we pass
 // this is compile time polymorphism
+
+public class MethodOverloading {
+
+    // same name, different parameters
+    static void print(int n) {
+        System.out.println("int: " + n);
+    }
+
+    static void print(String s) {
+        System.out.println("string: " + s);
+    }
+
+    static void print(int a, int b) {
+        System.out.println("two ints: " + a + " and " + b);
+    }
+
+    public static void main(String[] args) {
+        print(10);
+        print("kiran");
+        print(10, 20);
+    }
+}
