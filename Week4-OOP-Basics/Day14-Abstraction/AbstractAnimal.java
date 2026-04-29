935ff43269f63279a134f012f43807def8b416e3
M V Kiran <mada.kiran@s.amity.edu>
2026-04-29 22:47:45 +0530

    \

---
+++ Week4-OOP-Basics/Day14-Abstraction/AbstractAnimal.java
@@ -0,0 +1,4 @@
+// day 14 - abstract animal
+// every animal eats and sleeps - defined in abstract class
+// but every animal makes different sound - abstract method
+// child must define their own sound
