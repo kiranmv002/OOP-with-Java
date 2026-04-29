ab1c26bef15c81a5696736e1a942d3ddde12541f
M V Kiran <mada.kiran@s.amity.edu>
2026-04-29 23:13:12 +0530

    Update AbstractAnimal.java

--- Week4-OOP-Basics/Day14-Abstraction/AbstractAnimal.java
+++ Week4-OOP-Basics/Day14-Abstraction/AbstractAnimal.java
@@ -2,3 +2,44 @@
 // every animal eats and sleeps - defined in abstract class
 // but every animal makes different sound - abstract method
 // child must define their own sound
+
+abstract class Animal3 {
+
+    String name;
+
+    // abstract - every animal has different sound
+    abstract void sound();
+
+    // concrete - all animals eat and sleep
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+
+    void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+}
+
+class Dog3 extends Animal3 {
+
+    Dog3(String name) {
+        this.name = name;
+    }
+
+    @Override
+    void sound() {
+        System.out.println(name + " says woof!");
+    }
+}
+
+class Cat3 extends Animal3 {
+
+    Cat3(String name) {
+        this.name = name;
+    }
+
+    @Override
+    void sound() {
+        System.out.println(name + " says meow!");
+    }
+}
