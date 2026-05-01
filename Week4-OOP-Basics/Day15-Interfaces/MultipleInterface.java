0995d5bb6f0a67aa6e0009dd74b2e8a4289f73c6
M V Kiran <mada.kiran@s.amity.edu>
2026-05-01 22:09:02 +0530

    Create MultipleInterface.java

---
+++ Week4-OOP-Basics/Day15-Interfaces/MultipleInterface.java
@@ -0,0 +1,4 @@
+// day 15 - multiple interfaces
+// java doesnt support multiple inheritance with classes
+// but a class can implement multiple interfaces
+// this is how java handles multiple inheritance
