e871ca4e3fcb9203c2e454835bd58f63a96f1e1c
M V Kiran <mada.kiran@s.amity.edu>
2026-05-01 22:03:33 +0530

    Update Printable.java

--- Week4-OOP-Basics/Day15-Interfaces/Printable.java
+++ Week4-OOP-Basics/Day15-Interfaces/Printable.java
@@ -3,3 +3,46 @@
 // any class that implements it must define all its methods
 // interface has no body for methods
 // a class can implement multiple interfaces
+
+interface Printable {
+    void print(); // no body
+}
+
+interface Showable {
+    void show(); // no body
+}
+
+// implementing one interface
+class Document implements Printable {
+
+    String content;
+
+    Document(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("printing: " + content);
+    }
+}
+
+// implementing multiple interfaces
+class Photo implements Printable, Showable {
+
+    String name;
+
+    Photo(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("printing photo: " + name);
+    }
+
+    @Override
+    public void show() {
+        System.out.println("showing photo: " + name);
+    }
+}
