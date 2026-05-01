8170ff4b1ad2c3a6ed0ea094f2744f31b6172df8
M V Kiran <mada.kiran@s.amity.edu>
2026-05-01 22:01:28 +0530

    Create Printable.java

---
+++ Week4-OOP-Basics/Day15-Interfaces/Printable.java
@@ -0,0 +1,5 @@
+// day 15 - interfaces
+// interface is like a contract
+// any class that implements it must define all its methods
+// interface has no body for methods
+// a class can implement multiple interfaces
