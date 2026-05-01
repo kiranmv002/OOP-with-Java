79cb72079810a3bad7a5881ce241f4077f3fd5a9
M V Kiran <mada.kiran@s.amity.edu>
2026-05-01 22:10:50 +0530

    Update MultipleInterface.java

--- Week4-OOP-Basics/Day15-Interfaces/MultipleInterface.java
+++ Week4-OOP-Basics/Day15-Interfaces/MultipleInterface.java
@@ -2,3 +2,40 @@
 // java doesnt support multiple inheritance with classes
 // but a class can implement multiple interfaces
 // this is how java handles multiple inheritance
+
+interface Flyable {
+    void fly();
+}
+
+interface Swimmable {
+    void swim();
+}
+
+interface Runnable2 {
+    void run();
+}
+
+// duck can fly, swim and run
+class Duck implements Flyable, Swimmable, Runnable2 {
+
+    String name;
+
+    Duck(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(name + " is flying");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(name + " is swimming");
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " is running");
+    }
+}
