0b7dded060ad0f25a9465d20d85ebc03e4e50d24
M V Kiran <mada.kiran@s.amity.edu>
2026-05-17 19:56:40 +0530

    Update WordCount.java

--- Week4-OOP-Basics/ HashMap/WordCount.java
+++ Week4-OOP-Basics/ HashMap/WordCount.java
@@ -2,3 +2,25 @@
 // counting how many times each word appears
 // if word already in map, increase count
 // if not, add it with count 1
+
+import java.util.HashMap;
+
+public class WordCount {
+
+    public static void main(String[] args) {
+        String sentence = "java is fun java is easy java";
+        String[] words = sentence.split(" "); // split by space
+
+        HashMap<String, Integer> count = new HashMap<>();
+
+        for (String word : words) {
+            if (count.containsKey(word)) {
+                count.put(word, count.get(word) + 1); // increase count
+            } else {
+                count.put(word, 1); // first time seeing this word
+            }
+        }
+
+        System.out.println("word count: " + count);
+    }
+}
