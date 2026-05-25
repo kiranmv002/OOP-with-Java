0078c138ff97bf02e35ff32d0d8d924e6558f4b7
M V Kiran <mada.kiran@s.amity.edu>
2026-05-25 22:19:55 +0530

    Update MultipleException.java

--- Week4-OOP-Basics/Exception Handling/MultipleException.java
+++ Week4-OOP-Basics/Exception Handling/MultipleException.java
@@ -3,3 +3,29 @@
 // finally block runs always, even if exception happens
 // useful for closing files or connections
 
+import java.util.Scanner;
+
+public class MultipleException {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("enter a number: ");
+            int n = Integer.parseInt(sc.next()); // can throw NumberFormatException
+
+            int[] arr = new int[5];
+            arr[n] = 100; // can throw ArrayIndexOutOfBoundsException
+
+            System.out.println("value set at index " + n);
+
+        } catch (NumberFormatException e) {
+            System.out.println("thats not a number!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("index out of range, use 0 to 4");
+        } finally {
+            // this always runs
+            System.out.println("done. finally block executed.");
+        }
+    }
+}
