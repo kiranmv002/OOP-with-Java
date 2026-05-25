12c97fb400778586df1704ea9ecab720d5e178c9
M V Kiran <mada.kiran@s.amity.edu>
2026-05-25 22:17:30 +0530

    Create MultipleException.java

---
+++ Week4-OOP-Basics/Exception Handling/MultipleException.java
@@ -0,0 +1,5 @@
+// day 18 - multiple exceptions
+// we can catch different types of exceptions separately
+// finally block runs always, even if exception happens
+// useful for closing files or connections
+
